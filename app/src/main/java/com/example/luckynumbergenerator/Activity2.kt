0011a73b35e8562a68607f5a4b0dc54f749f0720
busrabasegmez <busrabasegmez@gmail.com>
2023-07-18 11:49:23 +0300

    adding share button functionality

--- app/src/main/java/com/example/luckynumbergenerator/Activity2.kt
+++ app/src/main/java/com/example/luckynumbergenerator/Activity2.kt
@@ -1,5 +1,6 @@
 package com.example.luckynumbergenerator
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -21,6 +22,10 @@ class Activity2 : AppCompatActivity() {
         var userName =receiveUserName()
         var luckyNumber = randomNumberGenerator()
         numberText.setText("" +luckyNumber)
+
+        shareButton.setOnClickListener(){
+            shareData(userName , luckyNumber)
+        }
     }
 
 
@@ -34,6 +39,15 @@ class Activity2 : AppCompatActivity() {
     fun randomNumberGenerator(): Int{
         val luckyNumber = Random().nextInt(1000)
         return luckyNumber
+    }
+
+    fun shareData(username:String, num :Int){
+
+        var i = Intent(Intent.ACTION_SEND)
+        i.setType("text/plaim")
+        i.putExtra(Intent.EXTRA_SUBJECT,""+ username + "is lucky today")
+        i.putExtra(Intent.EXTRA_TEXT, "his lucky number is ${num}")
+        startActivity(i)
     }
 }
 
